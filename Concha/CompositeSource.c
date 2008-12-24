22e863fe8951af33221c0af596e378a557eff9b3
jsloan <jsloan@656cdb67-cf5e-0410-80f1-8fdcbb4d2177>
2008-12-24 20:46:34 +0000

    Added CompositeSource.
    git-svn-id: svn://graphite/concha/trunk@3 656cdb67-cf5e-0410-80f1-8fdcbb4d2177

---
+++ Concha/CompositeSource.c
@@ -0,0 +1,48 @@
+/* vi: set ts=4 expandtab shiftwidth=4: */
+
+/**
+ * @file
+ *
+ * Copyright 2008 Digital Aggregates Corporation, Arvada CO 80001-0587 USA<BR>
+ * Licensed under the terms in README.h<BR>
+ * Chip Overclock <coverclock@diag.com><BR>
+ * http://www.diag.com/navigation/downloads/Concha.html<BR>
+ */
+
+#include "CompositeSource.h"
+
+int readCompositeSource(Source * that) {
+	CompositeSource * tp = (CompositeSource *)that;
+    int data;
+    data = readSource(tp->primary);
+    if ((data == EOF) && (tp->secondary != (Source *)0)) {
+        closeSource(tp->primary);
+        tp->primary = tp->secondary;
+        tp->secondary = (Source *)0;
+        data = readSource(tp->primary);
+    }
+	return data;
+}
+
+int pushCompositeSource(Source * that, char data) {
+	CompositeSource * tp = (CompositeSource *) that;
+    return pushSource(tp->primary, data);
+}
+
+int closeCompositeSource(Source * that) {
+	CompositeSource * tp = (CompositeSource *) that;
+    closeSource(tp->primary);
+    if (tp->secondary != (Source *)0) {
+        closeSource(tp->secondary);
+    }
+	return 0;
+}
+
+Source * openCompositeSource(CompositeSource * that, Source * primary, Source * secondary) {
+	that->source.read = &readCompositeSource;
+	that->source.push = &pushCompositeSource;
+	that->source.close = &closeCompositeSource;
+	that->primary = primary;
+	that->secondary = secondary;
+	return (Source *)that;
+}
---
+++ Concha/CompositeSource.h
@@ -0,0 +1,29 @@
+/* vi: set ts=4 expandtab shiftwidth=4: */
+#ifndef _H_COM_DIAG_CONCHA_COMPOSITESOURCE_
+#define _H_COM_DIAG_CONCHA_COMPOSITESOURCE_
+
+/**
+ * @file
+ *
+ * Copyright 2008 Digital Aggregates Corporation, Arvada CO 80001-0587 USA<BR>
+ * Licensed under the terms in README.h<BR>
+ * Chip Overclock <coverclock@diag.com><BR>
+ * http://www.diag.com/navigation/downloads/Concha.html<BR>
+ */
+
+#include "Source.h"
+
+typedef struct CompositeSource CompositeSource;
+
+struct CompositeSource {
+	Source source;
+	Source * primary;
+	Source * secondary;
+};
+
+extern Source * openCompositeSource(CompositeSource * that, Source * primary, Source * secondary);
+extern int readCompositeSource(Source * that);
+extern int pushCompositeSource(Source * that, char data);
+extern int closeCompositeSource(Source * that);
+
+#endif
