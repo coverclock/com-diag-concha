76ae25e3b2414ebdc580f1a734da5c8a45b8b141
jsloan <jsloan@656cdb67-cf5e-0410-80f1-8fdcbb4d2177>
2008-12-26 18:23:05 +0000

    Added NullSource and NullSink.
    git-svn-id: svn://graphite/concha/trunk/Concha@9 656cdb67-cf5e-0410-80f1-8fdcbb4d2177

---
+++ NullSource.c
@@ -0,0 +1,35 @@
+/* vi: set ts=4 expandtab shiftwidth=4: */
+
+/**
+ * @file
+ *
+ * Copyright 2008 Digital Aggregates Corporation, Arvada CO 80001-0587 USA<BR>
+ * Licensed under the terms in README.h<BR>
+ * Chip Overclock <coverclock@diag.com><BR>
+ * http://www.diag.com/navigation/downloads/Concha.html<BR>
+ */
+
+#include "NullSource.h"
+
+int readNullSource(Source * that) {
+	return EOF;
+}
+
+int pushNullSource(Source * that, char data) {
+	return EOF;
+}
+
+int closeNullSource(Source * that) {
+	return 0;
+}
+
+static SourceVirtualTable vtable = {
+	readNullSource,
+	pushNullSource,
+	closeNullSource
+};
+
+Source * openNullSource(NullSource * that) {
+	that->source.vp = &vtable;
+	return (Source *)that;
+}
