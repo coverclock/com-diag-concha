9511040c88068e7be95180b602c579b56ae57e7f
jsloan <jsloan@656cdb67-cf5e-0410-80f1-8fdcbb4d2177>
2009-01-01 17:20:01 +0000

    Added unittest-eleven which tests UnclosedSource and UnclosedSink.
    git-svn-id: svn://graphite/concha/trunk/Concha@24 656cdb67-cf5e-0410-80f1-8fdcbb4d2177

--- unittest-suite.sh
+++ unittest-suite.sh
@@ -4,21 +4,39 @@ export PATH=$PATH:.
 (
 	unittest-one < lesser.txt > output.txt || exit $?
 	diff lesser.txt output.txt || exit $?
+
 	unittest-two < lesser.txt > output.txt || exit $?
 	diff lesser.txt output.txt || exit $?
+
+	rm -f output*.txt
 	unittest-three || exit $?
 	diff lesser.txt output.txt || exit $?
+
 	unittest-four || exit $?
+
 	unittest-five || exit $?
+
+	rm -f output*.txt
 	unittest-six || exit $?
 	unittest-seven || exit $?
 	diff lesser.txt output.txt || exit $?
+
 	unittest-eight || exit $?
+
+	rm -f output*.txt
 	unittest-nine || exit $?
 	cat lesser.txt Makefile README.h > output3.txt
 	diff output.txt output3.txt || exit $?
+
+	rm -f output*.txt
 	unittest-ten || exit $?
 	diff lesser.txt output.txt || exit $?
+
+	rm -f output*.txt
+	unittest-eleven || exit $?
+	diff lesser.txt output.txt || exit $?
+
+	rm -f output*.txt
 	exit 0
 )
 RC=$?
