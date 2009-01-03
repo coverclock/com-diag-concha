4e274cbc2f04d4cfc4b287b38519c7d75a2db5b3
jsloan <jsloan@656cdb67-cf5e-0410-80f1-8fdcbb4d2177>
2009-01-03 19:28:56 +0000

    Added some echos to unittest-suite.
    git-svn-id: svn://graphite/concha/trunk/Concha@30 656cdb67-cf5e-0410-80f1-8fdcbb4d2177

--- unittest-suite.sh
+++ unittest-suite.sh
@@ -2,44 +2,58 @@
 export LD_LIBRARY_PATH=.
 export PATH=$PATH:.
 (
+	echo "1"
 	unittest-one < lesser.txt > output.txt || exit $?
 	diff lesser.txt output.txt || exit $?
 
+	echo "2"
 	unittest-two < lesser.txt > output.txt || exit $?
 	diff lesser.txt output.txt || exit $?
 
+	echo "3"
 	rm -f output*.txt
 	unittest-three || exit $?
 	diff lesser.txt output.txt || exit $?
 
+	echo "4"
 	unittest-four || exit $?
 
+	echo "5"
 	unittest-five || exit $?
 
+	echo "6"
 	rm -f output*.txt
 	unittest-six || exit $?
+	echo "7"
 	unittest-seven || exit $?
 	diff lesser.txt output.txt || exit $?
 
+	echo "8"
 	unittest-eight || exit $?
 
+	echo "9"
 	rm -f output*.txt
 	unittest-nine || exit $?
 	cat lesser.txt Makefile README.h > output3.txt
 	diff output.txt output3.txt || exit $?
 
+	echo "10"
 	rm -f output*.txt
 	unittest-ten || exit $?
 	diff lesser.txt output.txt || exit $?
 
+	echo "11"
 	rm -f output*.txt
 	unittest-eleven || exit $?
 	diff lesser.txt output.txt || exit $?
 
+	echo "11"
 	unittest-twelve || exit $?
 
+	echo "13"
 	unittest-thirteen || exit $?
 
+	echo "14"
 	rm -f output*.txt
 	unittest-fourteen || exit $?
 	diff lesser.txt output.txt || exit $?
